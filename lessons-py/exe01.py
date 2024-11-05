7eaeacea2d0a73f620dd28d510c979cf3efb079e
Moisés L. Pereira <125532933+JampaJ23@users.noreply.github.com>
2024-11-05 14:40:21 -0300

    exe-01

---
+++ lessons-py/exe01.py
@@ -0,0 +1,11 @@
+# Desafio:
+
+# nome = input('Qual é o seu nome?')
+# print ('Olá,' ,nome, '! Prazer em te cochecer.')
+
+# Estou agregando o input NOME a sintaxe, usando (,)
+# p/ encaixar com o restante do texte, re-utilizo (,) assim dando continuidade 
+
+nome = input('Qual seu nome?')
+cidade = input('Qual sua cidade?')
+print ('Olá,' ,nome,'seu endereço atual, é na cidade de' ,cidade, ', correto?')
