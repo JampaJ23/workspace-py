ce0cac572ccba8ec9b4a789d8664001cc0f5fbc4
Moisés L. Pereira <125532933+JampaJ23@users.noreply.github.com>
2024-11-05 15:43:54 -0300

    exe-guanabara

--- lessons-py/exe01.py
+++ lessons-py/exe01.py
@@ -6,6 +6,24 @@
 # Estou agregando o input NOME a sintaxe, usando (,)
 # p/ encaixar com o restante do texte, re-utilizo (,) assim dando continuidade 
 
-nome = input('Qual seu nome?')
-cidade = input('Qual sua cidade?')
+# Desafio: 01
+nome = input('Qual é o seu nome? ')
+print ('Olá' ,nome, '! Prazer em te conhecer.')
+
+# Desafio: 02
+nome = input('Qual seu nome? ')
+cidade = input('Qual sua cidade? ')
 print ('Olá,' ,nome,'seu endereço atual, é na cidade de' ,cidade, ', correto?')
+
+dia = input('Qual é o dia do seu nascimento? ')
+mes = input('Qual é o mes do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
+print ('O seu aniversário é ',dia,' de',mes,' de', ano,'. Correto?')
+
+# Desafio: 03
+n1 = int (input('Digite um numero a ser somado: '))
+n2 = int (input('Agora, o segundo numero: '))
+soma = n1 + n2 # -> soma = int(n1) + int(n2)
+print ("A soma dos valores" ,n1, "e" ,n2, "é:" ,soma,)
+# obs: int pode ser usado na linha do INPUT ou na SOMA dos valores.
+
---
+++ lessons-py/mindstorm.py
@@ -0,0 +1,6 @@
+# usar apenas para rodar codes em aprendizado!
+
+n1 = (input('Digite um numero a ser somado: '))
+n2 = (input('Agora, o segundo numero: '))
+soma = int(n1) + int(n2)
+print ("A soma dos valores" ,n1, "e" ,n2, "é:" ,soma,)
