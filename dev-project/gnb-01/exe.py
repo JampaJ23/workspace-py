f6c1082923cd5423ffbe8f25ad4cd500bde03a3c
Moisés L. Pereira <125532933+JampaJ23@users.noreply.github.com>
2024-11-06 09:33:47 -0300

    org

--- dev-project/gnb-01/exe.py
+++ dev-project/gnb-01/exe.py
@@ -1,29 +1,30 @@
-# 05-nov
+# 05-nov CV:01/05
 
 nome = input('Qual é o seu nome?')
-print ('Olá,' ,nome, '! Prazer em te cochecer.')
+print('Olá,' ,nome, '! Prazer em te cochecer.')
 # Estou agregando o input NOME a sintaxe, usando (,)
 # p/ encaixar com o restante do texte, re-utilizo (,) assim dando continuidade na sintaxe
 
 # Desafio: 01
 nome = input('Qual é o seu nome? ')
-print ('Olá' ,nome, '! Prazer em te conhecer.')
+print('Olá' ,nome, '! Prazer em te conhecer.')
 
 # Desafio: 02
 nome = input('Qual seu nome? ')
 cidade = input('Qual sua cidade? ')
-print ('Olá,' ,nome,'seu endereço atual, é na cidade de' ,cidade, ', correto?')
+print('Olá,' ,nome,'seu endereço atual, é na cidade de' ,cidade, ', correto?')
 
 dia = input('Qual é o dia do seu nascimento? ')
 mes = input('Qual é o mes do seu nascimento? ')
 ano = input('Qual o ano do seu nascimento? ')
-print ('O seu aniversário é ',dia,' de',mes,' de', ano,'. Correto?')
+print('O seu aniversário é ',dia,' de',mes,' de', ano,'. Correto?')
 
 # Desafio: 03
-n1 = int (input('Digite um numero a ser somado: '))
-n2 = int (input('Agora, o segundo numero: '))
+n1 = int(input('Digite um numero a ser somado: '))
+n2 = int(input('Agora, o segundo numero: '))
 soma = n1 + n2 # -> soma = int(n1) + int(n2)
-print ("A soma dos valores" ,n1, "e" ,n2, "é:" ,soma,)
+print("A soma dos valores" ,n1, "e" ,n2, "é:" ,soma,)
 # obs: int pode ser usado na linha do INPUT ou na SOMA dos valores.
 # usa (,) ou (+) são coisas diferentes p/ linguagem py
 # numeros devem ser identificados corretamente. Ex: str, int, float, bool, 2f, list
+
---
+++ dev-project/gnb-02/exe.py
@@ -0,0 +1,3 @@
+# 06-nov CV: 06/?
+
+print("Python ex-001/002")
