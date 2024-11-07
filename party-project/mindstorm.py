6acaaf8807453bfb9cc9bed562077e23107b16fe
Moisés L. Pereira <125532933+JampaJ23@users.noreply.github.com>
2024-11-07 08:54:16 -0300

    fix

--- party-project/mindstorm.py
+++ party-project/mindstorm.py
@@ -4,5 +4,7 @@ id = int(input('Digite seu ID: '))
 nome = input('Digite seu nome: ')
 salario = float(input('Qual sua renda hoje: '))
 brz = bool(input('Você é Brasileiro? (S para Sim, ou, N para Não)'))
-print(f'teste')
+print(f'Nome: {nome}\nID: {id}\nSalario: {salario}\nBrasileiro: {brz}') # Ainda nao aperfeiçoado.
+# Resposta bool, não está respondendo conforme esperado;
 
+print('test')
--- party-project/operadores.py
+++ party-project/operadores.py
@@ -2,3 +2,18 @@
 # Operadores e expressões aritméticas
 # Exemplos:
 
+n1 = float(input('Digite um numero: ')) 
+n2 = float(input('Digite o segundo numero: '))
+# Float foi chamado, pois, os valores poder ser decimais; (33.3, 9.5, 0.33)
+soma = n1 + n2
+subt = n1 - n2
+mult = n1 * n2
+div = n1 / n2 # Valor mais proximo, a ser dividido (QUOCIENTE).
+divr = n1 % n2 # Resultado do restante da divisão.
+
+print(f'A soma dos valores é: {soma}')
+print(f'A subtração dos valores é: {subt}')
+print(f'A multiplicação dos valores é: {mult}')
+print(f'A divisão dos valores é: {div}')
+print(f'O resto da divisão dos valores é: {divr}')
+
