847edf8340fcc5a99dbe507c15d053c849ca5713
Moisés L. Pereira <125532933+M-LucasPr@users.noreply.github.com>
2024-11-10 17:15:46 -0300

    fix

--- logica-python/f-strings.py
+++ logica-python/f-strings.py
@@ -6,7 +6,8 @@ a = 5
 b = 10 
 print(f"A soma de {a} e {b} é {a + b}.")
     # A soma de 5 e 10 é 15;
-    # {} abre e fecha os formatadores.
+    # {} abre e fecha os formatadores;
+    # Boas praticas: usar os valores mais claros possiveis, para os input().
 
 print(f"Preço: R$ {25.12345: .2f}")
     # f vem antes das (""), as quais carregam a informação;
@@ -43,3 +44,4 @@ print("Olá", "mundo", "!", sep="-")
 print("Olá", "mundo", "!", sep="_")
     # Saida: Olá_mundo_!
     # ou seja, permite controlar como os itens passados para a função print() são separados.
+
