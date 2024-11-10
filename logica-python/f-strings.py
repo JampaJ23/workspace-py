d67f58abc5d10a430524095a591840c6da7f5682
Moisés L. Pereira <125532933+M-LucasPr@users.noreply.github.com>
2024-11-10 14:09:16 -0300

    fix

--- logica-python/f-strings.py
+++ logica-python/f-strings.py
@@ -18,3 +18,28 @@ print(f"O valor em binario é: {23: b}") # binario
 print(f"Pi: {3.14159265: f}")
 print(f"Pi: {3.14159265: .2f}")
 
+numero_1 = 23
+numero_2 = 10
+numero_3 = 1993
+print(numero_1, numero_2, numero_3)
+print(numero_1, numero_2, numero_3, end="...\n")
+    # Saida: 23 10 1993...
+    # end= foi chamado para usar strings, logo apos os input, sem quebra de linha
+    # \n significa "new line"
+
+print("Início", end="-->")
+print("Meio", end="-->")
+print("Fim")
+    # Saida: Início-->Meio-->Fim
+    # Ou seja, pode trazer varias "print" na mesma line; sem quebra de linha
+
+print("Olá, mundo!", end=" ")
+print("Como você está?")
+    # Foi usado ESPAÇO apenas para separar os 'print' na saida;
+    # Saida: Olá, mundo! Como você está?
+
+print("Olá", "mundo", "!", sep="-")
+    # Saida: Olá-mundo-!
+print("Olá", "mundo", "!", sep="_")
+    # Saida: Olá_mundo_!
+    # ou seja, permite controlar como os itens passados para a função print() são separados.
