5325453bc359adc2d772f5b2d58c70a7a66160e9
Moisés L. Pereira <125532933+M-LucasPr@users.noreply.github.com>
2024-11-16 16:29:36 -0300

    fix saque

--- logica-python/brainstorming.py
+++ logica-python/brainstorming.py
@@ -34,8 +34,8 @@ saldo = 0
 SAQUE_CAIXA = 3
 SAQUE_LIMITE_CAIXA = 500
 SAQUE_LIMITE_DIA = 1500
-saque_limite_atual = 1500
-saque_caixa_atual = 3
+saque_caixa_diario = 3
+saque_dia = SAQUE_LIMITE_DIA / SAQUE_LIMITE_CAIXA
 
 
 while True:
@@ -45,27 +45,31 @@ while True:
     if opcao == 1:
         deposito = float(input('Digite o valor a ser depositado em sua conta: '))
         saldo += deposito
-        print(f'Seu novo saldo é de: {saldo: .2f}')
+        print(f'Seu novo saldo é de: {saldo}:')
         escolha = int(input('Deseja voltar ao menu? [1] SIM ou [2] para NÂO: '))
         if escolha > 1:
             print('Obrigado por usar nosso banco!')
             break
         else:
             menu
+
     if opcao == 2:
         sacar = float(input('Digite, o valor a ser sacado: '))
-        if saldo < sacar:
-            print(f'Valor insuficiente para saque, pois seu saldo é de {saldo: .2f}.')
-            escolha = int(input('Deseja voltar ao menu? [1] SIM ou [2] para NÂO: '))
-            if escolha > 1:
-                print('Obrigado por usar nosso banco!')
-                break
-        if (saldo > sacar and SAQUE_LIMITE_CAIXA < sacar):
+        if sacar < saldo and sacar < SAQUE_LIMITE_CAIXA:
             print(f'Seu novo saldo é de: R$ {saldo - sacar}')
             escolha = int(input('Deseja voltar ao menu? [1] SIM ou [2] para NÂO: '))
+            if escolha == 1:
+                print('Obrigado por usar nosso banco!')
+                menu
+            else:
+                break              
+        else:
+            print(f"Desculpe {usuario}, mas não foi possivel completar o saque. Pois seu saque de R$ {sacar}, é maior do que o saldo em sua conta ou seus limites. Verifique se o valor não excede seu limite diario de R$ {SAQUE_LIMITE_DIA} ou seu limite de saque em caixa de R$ {SAQUE_LIMITE_CAIXA}")
+            escolha = int(input('Deseja voltar ao menu? [1] SIM ou [2] para NÂO: '))
             if escolha > 1:
                 print('Obrigado por usar nosso banco!')
                 break
+
     if opcao == 3:
         print(f'Seu saldo atual é de: R$ {saldo: .2f}')
         escolha = int(input('Deseja voltar ao menu? [1] SIM ou [2] para NÂO: '))
